a4e3ad7a3ca60b34ca282577a91a78a605e2b44c
Abhi-2903 <abhimanyuchachan9@gmail.com>
2025-03-24 12:03:37 +0530

    Currency Converter

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -35,7 +35,7 @@ function InputBox({
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
-                <p className="text-black/40 mb-2 w-full">Currency Type</p>
+                <p className="text-black/40 mb-2 w-full">Currency </p>
                 <select
                     className="rounded-lg px-2 py-2 bg-gray-100 cursor-pointer outline-none"
                     value={selectCurrency}
